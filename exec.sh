9200fc210c3822967e7157718a4c3d2d84aa7430
Anderson <andersonmaliszewski@gmail.com>
2019-07-09 16:03:12 -0300

    delay

--- exec.sh
+++ exec.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 mpirun -np 2 -machinefile nodes is.S.x >> is.S.log  &
 sudo ./send.py h5 "OK" 600 &
+
 sleep 5
-ssh h6 iperf -c h2 -u -t 30 &
+ssh h6 iperf -c h2 -u -t 50 &
 sleep 55
+
 kisend=$(pidof python ./send.py)
 sudo kill -9 $kisend
